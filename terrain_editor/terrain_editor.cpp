02e0ba28896f180b2234e145cbb6cf15d2779fbf
Caleb Simpson <the.foner@gmail.com>
2014-09-17 12:23:06 -0400

    Add window resize handling to not mess up projection.

--- terrain_editor/terrain_editor.cpp
+++ terrain_editor/terrain_editor.cpp
@@ -8,23 +8,33 @@ void render()
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glBegin(GL_TRIANGLES);
 
-	glVertex3f(-0.5, -0.5f, 0.0);
-	glVertex3f(0.5, 0.0, 0.0);
-	glVertex3f(0.0, 0.5, 0.0);
+	glVertex3f(-2, -2, -5.0);
+	glVertex3f(2, 0.0, -5.0);
+	glVertex3f(0.0, 2, -5.0);
 
 	glEnd();
 	glutSwapBuffers();
 }
 
+void changeSize(int w, int h)
+{
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glViewport(0, 0, w, h);
+	gluPerspective(45, 1.0 * w / h, 1, 100);
+	glMatrixMode(GL_MODELVIEW);
+}
+
 int main(int argc, char **argv)
 {
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowPosition(100, 100);
 	glutInitWindowSize(640, 480);
 	glutCreateWindow("Terrain Editor");
 
 	glutDisplayFunc(render);
+	glutReshapeFunc(changeSize);
 	
 	glutMainLoop();
 	return 0;
